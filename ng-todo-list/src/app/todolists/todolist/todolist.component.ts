621ba646510e720af2b33bbea128257402edac7f
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-04 11:08:23 -0600

    📅  | Mon Apr  4 11:08:23 MDT 2022

--- ng-todo-list/src/app/todolists/todolist/todolist.component.ts
+++ ng-todo-list/src/app/todolists/todolist/todolist.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
-  styleUrls: ['./todolist.component.css']
+  styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
+  @Input()
+  name: string = 'list name';
 
-  constructor() { }
+  percentFinished: string = '0%';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- ng-todo-list/src/app/todolists/todolists.component.ts
+++ ng-todo-list/src/app/todolists/todolists.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { TodolistsStoreService } from '../services/todolists-store.service';
 
 @Component({
   selector: 'app-todolists',
   templateUrl: './todolists.component.html',
-  styleUrls: ['./todolists.component.css']
+  styleUrls: ['./todolists.component.css'],
 })
 export class TodolistsComponent implements OnInit {
+  todolists: any;
 
-  constructor() { }
+  constructor(public todolistsStore: TodolistsStoreService) {}
 
   ngOnInit(): void {
+    this.todolistsStore.getTodoLists().subscribe((value) => {
+      this.todolists = value;
+    });
   }
-
 }
