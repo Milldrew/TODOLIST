fdb70a123e77e4ff66b766d379b61e69047bd077
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-04 13:48:34 -0600

    📅  | Mon Apr  4 13:48:34 MDT 2022

--- ng-todo-list/src/app/view-todolist/view-todolist.component.ts
+++ ng-todo-list/src/app/view-todolist/view-todolist.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TodolistsStoreService } from '../services/todolists-store.service';
 
 @Component({
   selector: 'app-view-todolist',
   templateUrl: './view-todolist.component.html',
-  styleUrls: ['./view-todolist.component.css']
+  styleUrls: ['./view-todolist.component.css'],
 })
 export class ViewTodolistComponent implements OnInit {
-
-  constructor() { }
+  todolistData: any;
+  constructor(
+    private todolistStore: TodolistsStoreService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((value) => {
+      let listId = Number(value['id']);
+      console.log(value);
+      this.todolistData = this.todolistStore.getTodolistById(listId);
+    });
   }
-
 }
