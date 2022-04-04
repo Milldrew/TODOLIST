9d2d761e75cc82b8d28fb67de6c0d8c83fc53c35
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-04 10:18:38 -0600

    📅  | Mon Apr  4 10:18:38 MDT 2022

--- ng-todo-list/src/app/app.module.ts
+++ ng-todo-list/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistsComponent } from './todolists/todolists.component';
+import { TodolistComponent } from './todolists/todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistsComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
