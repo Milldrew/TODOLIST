c56db3ebbc17e59446bd3533e46e63852e159b50
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-04 12:26:43 -0600

    📅  | Mon Apr  4 12:26:43 MDT 2022

--- ng-todo-list/src/app/app-routing.module.ts
+++ ng-todo-list/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodolistsComponent } from './todolists/todolists.component';
+import { ViewTodolistComponent } from './view-todolist/view-todolist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'lists', pathMatch: 'full' },
+  { path: 'lists', component: TodolistsComponent },
+  { path: 'list/:id', component: ViewTodolistComponent },
+  { path: '**', redirectTo: 'lists' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
