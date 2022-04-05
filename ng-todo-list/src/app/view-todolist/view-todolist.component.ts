e8a7d30c47b380a1f208bbc5d47c9f679e3f1271
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-05 13:41:42 -0600

    📅  | Tue Apr  5 13:41:42 MDT 2022

--- ng-todo-list/src/app/view-todolist/view-todolist.component.ts
+++ ng-todo-list/src/app/view-todolist/view-todolist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TodoListApiService } from '../services/todo-list-api.service';
 import { TodolistsStoreService } from '../services/todolists-store.service';
 
 @Component({
@@ -10,19 +11,26 @@ import { TodolistsStoreService } from '../services/todolists-store.service';
 export class ViewTodolistComponent implements OnInit {
   todolistData: any;
   constructor(
+    private listApi: TodoListApiService,
     private todolistStore: TodolistsStoreService,
     private route: ActivatedRoute
   ) {}
 
   addTodo() {
     this.todolistData.todos.push({ name: 'New Todo', isFinished: false });
+    this.listApi.updateList(this.todolistData);
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe((value) => {
-      let listId = Number(value['id']);
-      console.log(value);
-      this.todolistData = this.todolistStore.getTodolistById(listId);
+    this.listApi.getLists().subscribe((lists) => {
+      this.todolistStore.setTodoLists(lists);
     });
+
+    setTimeout(() => {
+      this.route.params.subscribe((value) => {
+        let listId = Number(value['id']);
+        this.todolistData = this.todolistStore.getTodolistById(listId);
+      });
+    }, 1000);
   }
 }
