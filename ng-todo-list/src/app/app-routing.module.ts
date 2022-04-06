d79b7d89f09517d6f819a04e635e06bb03e9e9ba
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-05 19:54:27 -0600

    finished screens

--- ng-todo-list/src/app/app-routing.module.ts
+++ ng-todo-list/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { TodolistsComponent } from './todolists/todolists.component';
 import { ViewTodolistComponent } from './view-todolist/view-todolist.component';
@@ -8,6 +9,7 @@ import { ViewTodolistComponent } from './view-todolist/view-todolist.component';
 const routes: Routes = [
   { path: '', component: SignInComponent },
   { path: 'lists', component: TodolistsComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'list/:id', component: ViewTodolistComponent },
   { path: '**', redirectTo: 'lists' },
 ];
