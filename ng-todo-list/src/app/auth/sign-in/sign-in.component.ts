ddd3ec18fbc86ec4e6d9db96ad3bd52fa3a8c908
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-05 20:41:32 -0600

    📅  | Tue Apr  5 20:41:31 MDT 2022

--- ng-todo-list/src/app/auth/sign-in/sign-in.component.ts
+++ ng-todo-list/src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,11 +9,16 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class SignInComponent implements OnInit {
   @ViewChild('username') inputElement: ElementRef;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     setTimeout(() => {
       this.inputElement.nativeElement.focus();
     });
+  }
+
+  signIn() {
+    console.log('hello');
+    this.router.navigate(['lists']);
   }
 }
