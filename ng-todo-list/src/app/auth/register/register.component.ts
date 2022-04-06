39b47908eda954823a1338ddd32b0a62c8cc3da1
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-06 14:32:17 -0600

    📅  | Wed Apr  6 14:32:17 MDT 2022

--- ng-todo-list/src/app/auth/register/register.component.ts
+++ ng-todo-list/src/app/auth/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import { UserApiService } from 'src/app/services/user-api.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(@Inject(UserApiService) private userApi: UserApiService) {}
 
-  constructor() { }
+  registerForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  register() {
+    let createUserDto = this.registerForm.value;
+    console.log({ createUserDto });
+    this.userApi.registerUser(createUserDto);
   }
-
 }
