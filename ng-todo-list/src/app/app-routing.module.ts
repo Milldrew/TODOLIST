045c864bf96ebf0dd695ac58eb8f666d5d48ac4a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-05 20:37:46 -0600

    📅  | Tue Apr  5 20:37:46 MDT 2022

--- ng-todo-list/src/app/app-routing.module.ts
+++ ng-todo-list/src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { AuthGaurdService } from './guard/auth-gaurd.service';
 import { TodolistsComponent } from './todolists/todolists.component';
 import { ViewTodolistComponent } from './view-todolist/view-todolist.component';
 
 //{ path: '', redirectTo: 'lists', pathMatch: 'full' },
 const routes: Routes = [
   { path: '', component: SignInComponent },
-  { path: 'lists', component: TodolistsComponent },
+  {
+    path: 'lists',
+    component: TodolistsComponent,
+    canActivate: [AuthGaurdService],
+  },
   { path: 'register', component: RegisterComponent },
   { path: 'list/:id', component: ViewTodolistComponent },
   { path: '**', redirectTo: 'lists' },
