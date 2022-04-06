eb9cc4bb065682da161e8f0ee486c4a7ef98e796
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-05 20:30:07 -0600

    📅  | Tue Apr  5 20:30:07 MDT 2022

--- ng-todo-list/src/app/guard/auth-gaurd.service.ts
+++ ng-todo-list/src/app/guard/auth-gaurd.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGaurdService {
+export class AuthGaurdService implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor() { }
+  canActivate(): boolean {
+    let isLoggedIn = this.userService.getIsAuthenticated();
+    console.log('AuthGaurd canActivate called');
+
+    if (isLoggedIn) {
+      return true;
+    }
+
+    console.log('not authorized to acess page by auth gaurd');
+
+    return false;
+  }
 }
