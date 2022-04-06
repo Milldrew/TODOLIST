d51500c4cb5c7bfacea6925e89d6a3fb81032bf0
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-06 14:58:31 -0600

    📅  | Wed Apr  6 14:58:31 MDT 2022

--- ng-todo-list/src/app/services/user-api.service.ts
+++ ng-todo-list/src/app/services/user-api.service.ts
@@ -8,7 +8,14 @@ export class UserApiService {
   constructor(private http: HttpClient) {}
 
   userUrl = `http://192.168.0.143:3000/user`;
+  loginUrl = `http://192.168.0.143:3000/auth/login`;
   registerUser(createUserDto: any) {
     return this.http.post(this.userUrl, createUserDto);
+  }
+
+  signIn(username: string, password: string) {
+    return this.http
+      .post(this.loginUrl, { username, password })
+      .subscribe((payload) => console.log(payload));
   }
 }
