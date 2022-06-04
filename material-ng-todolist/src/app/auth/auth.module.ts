684466343a3602884f72fae21cca1488ee1e9fb8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-04 16:07:50 -0600

    📅  | Sat Jun  4 16:07:50 MDT 2022

--- material-ng-todolist/src/app/auth/auth.module.ts
+++ material-ng-todolist/src/app/auth/auth.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RegisterComponent } from './register/register.component';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    SignInComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SignInComponent, RegisterComponent],
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatInputModule],
+  exports: [SignInComponent, RegisterComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
