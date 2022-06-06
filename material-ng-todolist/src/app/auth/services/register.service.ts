7f3bf2b6244f8c60421ea6529852e8d84226d5f3
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-06 11:00:26 -0600

    📅  | Mon Jun  6 11:00:26 MDT 2022

--- material-ng-todolist/src/app/auth/services/register.service.ts
+++ material-ng-todolist/src/app/auth/services/register.service.ts
@@ -1,19 +1,29 @@
+import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterDto } from '../models/register-dto';
+import { UserService } from 'src/app/core/services/user.service';
+import { SignInService } from './sign-in.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RegisterService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly userService: UserService,
+    private readonly signInService: SignInService
+  ) {}
 
   register(registerDto: RegisterDto) {
-    this.http
-      .post(registerDto, {
-        username: 'FROM MATERIAL CLIENT',
-        password: 'PASSWORD FROM MATERIAL CLIENT',
-      })
-      .subscribe(console.log, console.error, console.log);
+    console.log({ registerDto });
+    this.http.post(`${environment.baseUrl}/user`, registerDto).subscribe(
+      (payload) => {
+        console.log(payload);
+        this.userService.setUser(payload);
+      },
+      console.error,
+      console.log
+    );
   }
 }
--- material-ng-todolist/src/app/auth/services/sign-in.service.ts
+++ material-ng-todolist/src/app/auth/services/sign-in.service.ts
@@ -1,9 +1,15 @@
+import { SignInDto } from '../models/sign-in-dto';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignInService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  signIn(signInDto: SignInDto) {
+    this.http.post(`${environment.baseUrl}/auth/login`, signInDto);
+  }
 }
