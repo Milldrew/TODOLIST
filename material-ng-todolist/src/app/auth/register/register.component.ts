868387a11a0e68469a8ea00f2443968d7c3d0a1a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-06 09:23:40 -0600

    📅  | Mon Jun  6 09:23:40 MDT 2022

--- material-ng-todolist/src/app/auth/register/register.component.ts
+++ material-ng-todolist/src/app/auth/register/register.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
-  constructor() { }
+  matcher = new AuthErrorStateMatcher();
+  constructor() {}
+  passwordToggleValue: 'text' | 'password' = 'text';
 
-  ngOnInit(): void {
+  togglePassword() {
+    console.log(this.passwordToggleValue);
+    this.passwordToggleValue === 'text'
+      ? (this.passwordToggleValue = 'password')
+      : (this.passwordToggleValue = 'text');
   }
 
+  ngOnInit(): void {}
+}
+
+export class AuthErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
 }
