75620aaa10d1c0517c0a6f653a5a9da85bef38d1
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-06 09:53:22 -0600

    📅  | Mon Jun  6 09:53:22 MDT 2022

--- material-ng-todolist/src/app/auth/services/register.service.ts
+++ material-ng-todolist/src/app/auth/services/register.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RegisterDto } from '../models/register-dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegisterService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  register(registerDto: RegisterDto) {
+    this.http
+      .post(registerDto, {
+        username: 'FROM MATERIAL CLIENT',
+        password: 'PASSWORD FROM MATERIAL CLIENT',
+      })
+      .subscribe(console.log, console.error, console.log);
+  }
 }
