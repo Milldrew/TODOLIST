b4aacc45cfc46f42363a2693e0551c535eb9c4bb
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-08 16:15:30 -0600

    📅  | Wed Jun  8 16:15:30 MDT 2022

--- material-ng-todolist/src/app/display-lists/view-list/add-todo-overlay/add-todo-overlay.component.ts
+++ material-ng-todolist/src/app/display-lists/view-list/add-todo-overlay/add-todo-overlay.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo-overlay',
   templateUrl: './add-todo-overlay.component.html',
-  styleUrls: ['./add-todo-overlay.component.scss']
+  styleUrls: ['./add-todo-overlay.component.scss'],
 })
 export class AddTodoOverlayComponent implements OnInit {
+  name: string | null = null;
 
-  constructor() { }
+  @Output()
+  closeWindow = new EventEmitter<boolean>();
+  @Output()
+  newName = new EventEmitter<string>();
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submitName() {
+    console.log(this.name);
+    if (this.name) this.newName.emit(this.name);
+    this.closeWindow.emit(false);
   }
 
+  close() {
+    this.closeWindow.emit(false);
+  }
 }
